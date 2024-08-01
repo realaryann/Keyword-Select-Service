c7baf7977f83fd4b217efc55fe57400a267677c8
aryan <arcnagpal1234@gmail.com>
2024-08-01 10:06:44 -0400

    Overhauled updation

--- hubo_voice_command/hubo_voice_command/main.py
+++ hubo_voice_command/hubo_voice_command/main.py
@@ -25,24 +25,34 @@ class KeyService(Node):
                 self.send_goal('rest')
             elif ins == 'transform':
                 self.send_goal('transform')
+            elif ins == 'stop':
+                self.send_goal('stop')
         return response
 
     def send_goal(self, text):
         goal_msg = Huboaction.Goal()
         goal_msg.msg = text
+        if text == 'stop':
+            return;
+        else:
+            self._action_client.wait_for_server()
+            self._send_goal_future = self._action_client.send_goal_async(
+                goal_msg, feedback_callback=self.feedback_callback)
 
-        self._action_client.wait_for_server()
-        self._send_goal_future = self._action_client.send_goal_async(
-            goal_msg, feedback_callback=self.feedback_callback)
+            self._send_goal_future.add_done_callback(self.goal_response_callback)
 
-        self._send_goal_future.add_done_callback(self.goal_response_callback)
+    def goal_canceled_callback(self, future):
+        cancel_response = future.result()
+        if len(cancel_response.goals_canceling) > 0:
+            self.get_logger().info('Cancelling of goal complete')
+        else:
+            self.get_logger().warning('Goal failed to cancel')
 
     def goal_response_callback(self, future):
         goal_handle = future.result()
         if not goal_handle.accepted:
             self.get_logger().info('Goal rejected')
             return
-
         self.get_logger().info('Goal accepted')
 
         self._get_result_future = goal_handle.get_result_async()
